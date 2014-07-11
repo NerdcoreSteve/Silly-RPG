fda802d3e2b74d61537bdbcbc75553e5af010958
Test Account <testaccount@Steve-Smiths-Mac-mini.local>
2014-07-11 13:29:01 -0600

    added new files FOR REAL this time

---
+++ common.py
@@ -0,0 +1,9 @@
+#silly rpg common code
+
+#These directions are the only ones that exist in the game world
+#The functions below, change_speed and opposite_velocity, work on this assumption
+right   = [ 1,  0]
+left    = [-1,  0]
+up      = [ 0, -1]
+down    = [ 0,  1]
+stopped = [ 0,  0]
