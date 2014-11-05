a1c659946827cb661b27f7c65cb5bdd211ab3531
Steve <steve@Steve-Linux.(none)>
2014-11-05 12:32:07 -0700

    added z-index modification with arrow keys and cursor in editor

--- editor.py
+++ editor.py
@@ -23,6 +23,26 @@ class Cursor(object):
         if self.current_field_element:
             self.rect = self.current_field_element.rect
 
+class Editor_Field(common.Field):
+    def __init__(self, field_data):
+        common.Field.__init__(self, field_data)
+
+    def move(self, field_element, direction):
+        move_map =   {"up": lambda index: index + 1, "down": lambda index: index - 1}
+        bounds_map = {"up": lambda index: index >= len(self.field_elements), "down": lambda index: index < 0}
+        if direction == "up":
+            old_index = self.field_elements.index(field_element)
+            new_index = move_map[direction](old_index);
+            if bounds_map["up"](new_index):
+                new_index = old_index
+            self.field_elements.insert(new_index, self.field_elements.pop(old_index))
+        elif direction == "down":
+            old_index = self.field_elements.index(field_element)
+            new_index = move_map[direction](old_index);
+            if bounds_map["up"](new_index):
+                new_index = old_index
+            self.field_elements.insert(new_index, self.field_elements.pop(old_index))
+
 def render(screen, cursor):
     screen.fill(background_color)
     field.blit(screen)
@@ -42,7 +62,7 @@ screen = pygame.display.set_mode(game_data["screen size"])
 background_color = 0, 0, 0
 frame_rate = 60
 player = common.Player(game_data["screen size"], game_data["player"])
-field = common.Field(game_data["field"])
+field = Editor_Field(game_data["field"])
 clock = pygame.time.Clock()
 cursor = Cursor()
 render(screen, cursor)
@@ -55,6 +75,12 @@ while True:
     elif event.type == pygame.KEYDOWN:
         if event.key == pygame.K_ESCAPE:
             sys.exit()
+        elif event.key == pygame.K_DOWN:
+            if(clicked_field_element):
+                field.move(clicked_field_element, "down")
+        elif event.key == pygame.K_UP:
+            if(clicked_field_element):
+                field.move(clicked_field_element, "up")
     elif event.type == pygame.MOUSEBUTTONDOWN:
         if event.dict['button'] == 1:
             clicked_field_element = get_clicked_field_element(field, event.dict['pos'])
